64ed127ae86f19d148cde3317d26d6d2a37f25ac
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-09-20 10:27:46 -0400

    Update concat_mc.py

--- concat_mc.py
+++ concat_mc.py
@@ -1,1 +1,28 @@
+# Keily Drogt
+# 20 Sept 2024
+# Concatenation check
 
+# Task 1
+
+first_name = "Keily's"
+fav_junk_food = "Qudoba"
+
+sentence = first_name + " favorite junk food is" + fav_junk_food
+
+print(sentence)
+
+
+# Task 2
+
+print(f"What I\'ve learned about concatenations:")
+print(f"I\'ve learned that there is different ways to make concatenations in my code.")
+print(f"Next, I learned that you need to add an extra space so that the words aren\'t all connected in the sentence.")
+
+
+# Task 3
+
+bk_price = "$3.49"
+
+bk = "Burger King has double bacon cheeseburgers for only " + str(bk_price)
+
+print(bk)
